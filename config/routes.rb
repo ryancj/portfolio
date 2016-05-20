1ee1e2415fd20e2bdbdc5e770fffe84f5534a7c7
Ryan Kayu Ching <ryanching@hotmail.com>
2016-05-19 18:53:48 -0700

    Navbar and links controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :links
+
+  root 'links#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
