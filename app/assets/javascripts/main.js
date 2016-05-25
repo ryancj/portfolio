c0c57b59865d5639b82a85f3351f7056c34714e8
Ryan Kayu Ching <ryanching@hotmail.com>
2016-05-25 16:57:39 -0700

    Implement modals for the projects

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,4 +4,7 @@ $(function() {
 
   $(".button-collapse").sideNav(); //Side Nav for mobile
 
+  $('.materialboxed').materialbox(); //Image effect
+
+  $('.modal-trigger').leanModal(); //Initialize modals
 });
