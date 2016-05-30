9908ffd46c6d661ed17df8b3aad2e7daa0cf44e9
Ryan Kayu Ching <ryanching@hotmail.com>
2016-05-30 14:45:27 -0700

    Major styling changes

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,10 +10,10 @@ $(function() {
 
   $('.parallax').parallax(); //Initialize parallax
 
-  window.sr = ScrollReveal({ reset: true });
+  window.sr = ScrollReveal({ reset: true }); //Initialize ScrollReveal
 
-  sr.reveal('.tech-logo', { duration: 2000, distance: '20px' }, 50);
-  sr.reveal('.fade', { duration: 1000, distance:'20px', scale: 1 });
-  sr.reveal('.stag-list', { duration: 800, origin: 'left', distance:'20px', scale: 1 }, 100);
+  sr.reveal('.tech-logo', { duration: 1000, distance: '20px', viewFactor: 0.5 }, 45);
+  sr.reveal('.fade', { duration: 700, distance:'20px', scale: 1 });
+  sr.reveal('.stag-list', { duration: 700, origin: 'left', distance:'20px', scale: 1 }, 80);
 
 });
