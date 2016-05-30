d96fdbc4396ff08b9365515a3e1ae52ed81e04bc
Ryan Kayu Ching <ryanching@hotmail.com>
2016-05-30 15:48:59 -0700

    MORE STYLING

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -12,8 +12,8 @@ $(function() {
 
   window.sr = ScrollReveal({ reset: true }); //Initialize ScrollReveal
 
-  sr.reveal('.tech-logo', { duration: 1000, distance: '20px', viewFactor: 0.5 }, 45);
+  sr.reveal('.tech-logo', { duration: 1100, distance: '20px', viewFactor: 0.5 }, 40);
+  sr.reveal('.stag-list', { duration: 1000, origin: 'left', distance:'20px', scale: 1 }, 100);
   sr.reveal('.fade', { duration: 700, distance:'20px', scale: 1 });
-  sr.reveal('.stag-list', { duration: 700, origin: 'left', distance:'20px', scale: 1 }, 80);
-
+$('ul.tabs').tabs('select_tab', 'tab_id');
 });
