70ef48fc5335193fc5fff4688fd77f29f1b23a1c
Ryan Kayu Ching <ryanching@hotmail.com>
2016-05-30 18:51:42 -0700

    Nearly finish portfolio section

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -14,7 +14,7 @@ $(function() {
 
   sr.reveal('.tech-logo', { duration: 1100, distance: '20px', viewFactor: 0.5 }, 40);
   sr.reveal('.stag-list', { duration: 1000, distance:'20px', scale: 1 }, 100);
-  sr.reveal('.photo-fade', { duration: 700, distance:'20px', scale: 1 }, 40);
+  sr.reveal('.photo-fade', { duration: 800, distance:'20px', scale: 1 }, 40);
   sr.reveal('.fade', { duration: 700, distance:'20px', scale: 1 });
 $('ul.tabs').tabs('select_tab', 'tab_id');
 });
